389386b0b069b4ca25dec96cb1266ef6cb2dc6b2
mkshsmwl10-web <mkshsmwl10@gmail.com>
2026-05-31 13:02:41 +0530

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -41,7 +41,9 @@ spreadsheet_id = "1bNXvVoDXgBmB-R_w6nJr4sBVYK6bksrv35BVYkiNe2E"
 worksheet = client.open_by_key(
     spreadsheet_id
 ).worksheet("Top 250 Stocks")
-
+macd_sheet = client.open_by_key(
+    spreadsheet_id
+).worksheet("MACD_DATA")
 # =========================
 # 3. NSE Bhavcopy Fetcher
 # =========================
@@ -210,7 +212,31 @@ if data_to_insert:
             'A2',
             data_to_insert
         )
+# =========================
+# Save History For MACD
+# =========================
 
+today_db = datetime.now().strftime("%Y-%m-%d")
+
+history_rows = []
+
+for row in data_to_insert:
+
+    symbol = row[0]
+    close_price = row[2]
+
+    history_rows.append([
+        today_db,
+        symbol,
+        close_price
+    ])
+
+if history_rows:
+
+    macd_sheet.append_rows(
+        history_rows,
+        value_input_option='RAW'
+    )
         # Status message
         ist_now = (
             datetime.utcnow() +
