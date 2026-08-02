2679a459c0d3116ee07d4bd1c061f5dfc232d3b2
mkshsmwl10-web <mkshsmwl10@gmail.com>
2026-08-02 21:50:01 +0530

    Update etf_weekly_sip.py

--- etf_weekly_sip.py
+++ etf_weekly_sip.py
@@ -1,19 +1,43 @@
+import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-SPREADSHEET_ID = "13Jy8xJB9l6SQ124aEIAF3wFJRvJIUXX6jOH61rFb3zY"
+# ==========================
+# Google Authentication
+# ==========================
+
+creds_json = os.environ.get("GCP_CREDENTIALS")
+
+if not creds_json:
+    raise Exception("GCP_CREDENTIALS Secret Missing")
+
+creds_dict = json.loads(creds_json)
 
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "service_account.json", scope
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    creds_dict,
+    scope
 )
 
 client = gspread.authorize(creds)
 
+# ==========================
+# Open ETF Sheet
+# ==========================
+
+SPREADSHEET_ID = "13Jy8xJB9l6SQ124aEIAF3wFJRvJIUXX6jOH61rFb3zY"
+
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet("Sheet2")
 
-print(sheet.acell("A2").value)
+# Read ETF Codes from Column A
+symbols = sheet.col_values(1)[1:]
+
+print(f"Total ETFs Found : {len(symbols)}")
+
+for s in symbols:
+    print(s)
