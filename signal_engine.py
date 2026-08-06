c00b8307c600f889da4d75d907da915b94c71b82
mkshsmwl10-web <mkshsmwl10@gmail.com>
2026-08-06 19:21:03 +0530

    Update signal_engine.py

--- signal_engine.py
+++ signal_engine.py
@@ -148,6 +148,25 @@ print("Daily Confirmation Loaded")
 # DAILY MACD CROSS DETECTOR
 # ======================================
 
+# ======================================
+# DAILY POSITIVE
+# ======================================
+
+def daily_positive(d0):
+
+    if d0 is None:
+        return "NO"
+
+    if d0 > 0:
+        return "YES"
+
+    return "NO"
+
+
+# ======================================
+# DAILY FRESH CROSS
+# ======================================
+
 def daily_cross(d1, d0):
 
     if d1 is None or d0 is None:
@@ -158,6 +177,7 @@ def daily_cross(d1, d0):
 
     return "NO"
 
+print("Daily Signal Engine Loaded")
 
 print("Daily Cross Detector Loaded")
 
@@ -190,42 +210,38 @@ for _, row in df.iterrows():
 
     weekly = weekly_turn(w2, w1, w0)
 
-    daily = daily_cross(d1, d0)
+   daily_positive_signal = daily_positive(d0)
 
+daily_cross_signal = daily_cross(d1, d0)
 
     score = 0
+signal = "WAIT"
+
+# Weekly Turn
+if weekly == "TURN UP":
+    score += 60
+
+# Daily Positive
+if daily_positive_signal == "YES":
+    score += 40
+
+# Final Signal
+if score == 100:
+    signal = "BUY"
+
+elif score >= 60:
+    signal = "WATCH"
+
+else:
     signal = "WAIT"
 
 
-    # Weekly confirmation
-    if weekly == "TURN UP":
-        score += 2
-
-
-    # Daily confirmation
-    if daily == "YES":
-        score += 2
-
-
-    # Final Decision
-
-    if score >= 4:
-        signal = "STRONG BUY"
-
-    elif score >= 2:
-        signal = "WATCH"
-
-    else:
-        signal = "WAIT"
-
-
-    output.append([
-        weekly,
-        daily,
-        signal,
-        score
-    ])
-
+   output.append([
+    weekly,
+    daily_positive_signal,
+    signal,
+    score
+])
 
 sheet.update(
     range_name="K2:N201",
