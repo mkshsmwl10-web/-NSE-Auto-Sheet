5ae5a923b60d7fbad5b18975ef80bd3124a14b44
mkshsmwl10-web <mkshsmwl10@gmail.com>
2026-08-06 14:28:09 +0530

    Update signal_engine.py

--- signal_engine.py
+++ signal_engine.py
@@ -9,6 +9,16 @@ from oauth2client.service_account import ServiceAccountCredentials
 # GOOGLE LOGIN
 # ======================================
 
+sheet = client.open_by_key(
+    SPREADSHEET_ID
+).worksheet("MACD200")
+
+data = sheet.get_all_records()
+
+print(f"Loaded {len(data)} Stocks")
+
+if len(data) == 0:
+    raise Exception("MACD200 Sheet is Empty")
 creds_json = os.environ.get("GCP_CREDENTIALS")
 
 creds_dict = json.loads(creds_json)
@@ -25,7 +35,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_dict(
 
 client = gspread.authorize(creds)
 
-SPREADSHEET_ID = "1bNXvVoDXgBmB-R_w6nJr4sBVYK6E"
+SPREADSHEET_ID = "1bNXvVoDXgBmB-R_w6nJr4sBVYK6bksrv35BVYkiNe2E"
 
 sheet = client.open_by_key(
     SPREADSHEET_ID
